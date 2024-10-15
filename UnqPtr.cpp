d2b723c0589ce83059f8fc840fa7ceb08d98a2c7
Vitaliy Koldashev <vita-rzn@yandex.ru>
2024-10-16 00:32:45 +0300

    no i can't say anything

--- UnqPtr.cpp
+++ UnqPtr.cpp
@@ -1,12 +1,15 @@
 #include "UnqPtr.hpp"
+#include "MyDeleter.hpp"
 #include <stdio.h>
 
-
-template<typename T, >
+template<typename T, typename Ptr_Deleter = Deleter<T>>
 class UnqPtr
 {
 private:
-    T* ptr;
+    using element_type = T;
+    using pointer = element_type*;
+    using deleter_type = Ptr_Deleter;
+    
 public:
     
 };
