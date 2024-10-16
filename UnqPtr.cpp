a96575ce4e4ba9bdae5b46e0ba7ed24139d5e149
Vitaliy Koldashev <vita-rzn@yandex.ru>
2024-10-17 01:31:37 +0300

    added constructors and deleters

--- UnqPtr.cpp
+++ UnqPtr.cpp
@@ -1,6 +1,7 @@
 #include "UnqPtr.hpp"
 #include "MyDeleter.hpp"
 #include <stdio.h>
+#include <utility>
 
 template<typename T, typename Ptr_Deleter = Deleter<T>>
 class UnqPtr
@@ -9,7 +10,21 @@ private:
     using element_type = T;
     using pointer = element_type*;
     using deleter_type = Ptr_Deleter;
-    
+    pointer ptr;
+    deleter_type del = deleter_type();
 public:
+    UnqPtr(): ptr(nullptr) {};
+    UnqPtr(pointer other):ptr(std::move(other)) {};
+    UnqPtr(const UnqPtr<element_type, deleter_type>& other) = delete;
+    UnqPtr(UnqPtr<element_type, deleter_type>&& other):ptr(other)
+    {
+        other.ptr = nullptr;
+    }
+    virtual ~UnqPtr()
+    {
+        del(ptr);
+    }
+    
+    
     
 };
