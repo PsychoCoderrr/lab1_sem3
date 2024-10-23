8e36b23b5a9c487fd18a95a05f79f910a4d7c739
Vitaliy Koldashev <vita-rzn@yandex.ru>
2024-10-23 19:42:36 +0300

    added simple version of UI

---
+++ Ui.cpp
@@ -0,0 +1,8 @@
+//
+//  Ui.cpp
+//  labortory1
+//
+//  Created by Виталий Колдашев on 23.10.2024.
+//
+
+#include "Ui.hpp"
---
+++ Ui.hpp
@@ -0,0 +1,13 @@
+//
+//  Ui.hpp
+//  labortory1
+//
+//  Created by Виталий Колдашев on 23.10.2024.
+//
+
+#ifndef Ui_hpp
+#define Ui_hpp
+
+#include <stdio.h>
+
+#endif /* Ui_hpp */
